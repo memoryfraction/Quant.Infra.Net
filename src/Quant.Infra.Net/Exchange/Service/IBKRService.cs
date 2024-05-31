463026b4686524b34d8b248436eaacab1665d2d4
Rong(Rex) Fan || 樊荣 <rong.fan2020@outlook.com>
2024-05-31 17:23:23 -0400

    1 升级Quant.Infra.Net 为.net 6版本 2 实现了方法public Task PlaceMarketOrderAsync(Order order, string exchange = "SMART", ContractSecurityType securityType = ContractSecurityType.Stock, Currency currency = Currency.USD) 3 添加了InterReact的project reference 引用

--- src/Quant.Infra.Net/Exchange/Service/IBKRService.cs
+++ src/Quant.Infra.Net/Exchange/Service/IBKRService.cs
@@ -1,16 +1,61 @@
-﻿using Quant.Infra.Net.Shared.Model;
+﻿using AutoMapper;
+using InterReact;
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Quant.Infra.Net.Exchange.Service
 {
     public class IBKRService : IIBKRService
     {
-        public Task PlaceMarketOrderAsync(Order order, string exchange = "SMART", ContractSecurityType securityType = ContractSecurityType.Stock, Currency currency = Currency.USD)
+        private string _apiKey, _apiSecret;
+        private IMapper _mapper;
+        private IInterReactClient? _client;
+
+        public IBKRService(IMapper mapper)
         {
-            throw new NotImplementedException();
+            _mapper = mapper;
+            if(_client == null)
+                _client = InterReactClient.ConnectAsync().Result;
         }
+
+        public Task PlaceMarketOrderAsync(
+            Order order, string exchange = "SMART", 
+            Quant.Infra.Net.Shared.Model.ContractSecurityType securityType = Quant.Infra.Net.Shared.Model.ContractSecurityType.Stock, 
+            Quant.Infra.Net.Shared.Model.Currency currency = Quant.Infra.Net.Shared.Model.Currency.USD
+            )
+        {
+            // https://github.com/dshe/InterReact/blob/master/InterReact.Tests/SystemTests/Orders/PlaceOrderTests.cs
+
+            return Task.Run(() =>
+            {
+                // Your existing code...
+
+                InterReact.Contract interReactContract = new()
+                {
+                    SecurityType = InterReact.ContractSecurityType.Stock,
+                    Symbol = "AMZN",
+                    Currency = "USD",
+                    Exchange = "SMART"
+                };
+
+                int orderId = _client.Request.GetNextId();
+
+                InterReact.Order interReactOrder = new()
+                {
+                    //todo need change
+                    Action = OrderAction.Buy,
+                    TotalQuantity = 100,
+                    OrderType = OrderTypes.Market
+                };
+
+                _client.Request.PlaceOrder(orderId, interReactOrder, interReactContract);
+            });
+
+ 
+        }
+
+        
+
     }
 }

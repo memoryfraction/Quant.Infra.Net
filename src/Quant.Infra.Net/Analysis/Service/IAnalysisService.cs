b0c3ed29366bf75978f5a72c3f6ea7762370e481
Rong(Rex) Fan || 樊荣 <rong.fan2020@outlook.com>
2024-07-17 22:12:03 -0400

    新增时间序列分析方法 1 相关性 2 StationaryTest 3 OLS 线性回归

---
+++ src/Quant.Infra.Net/Analysis/Service/IAnalysisService.cs
@@ -0,0 +1,9 @@
+﻿namespace Quant.Infra.Net.Analysis.Service
+{
+    public interface IAnalysisService
+    {
+        double CalculateCorrelation(double[] seriesA, double[] seriesB);
+        bool PerformADFTest(double[] timeSeries, double threshold = 0.05);
+        (double Slope, double Intercept) PerformLinearRegression(double[] seriesA, double[] seriesB);
+    }
+}

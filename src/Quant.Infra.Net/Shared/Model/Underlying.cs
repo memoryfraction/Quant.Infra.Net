377da7e4ae73321ac8f3be6773ef7769165352dd
Rong Fan <rong.fan1031@gmail.com>
2024-09-10 21:17:09 -0400

    1 实现了Slice的部分框架 2 DataStreamManager新增Underlyings 3 CreateStrategy移除参数Portfolio 4 新增数据结构Underlying

---
+++ src/Quant.Infra.Net/Shared/Model/Underlying.cs
@@ -0,0 +1,8 @@
+﻿namespace Quant.Infra.Net.Shared.Model
+{
+    public class Underlying
+    {
+        public string Symbol { get; set; }
+        public AssetType AssetType { get; set; }
+    }
+}

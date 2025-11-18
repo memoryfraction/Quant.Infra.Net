7af727de73f2b4ebe2febb58429bcd838f39a758
THINKPAD_P14S\rongf <rex.fan18@gmail.com>
2025-11-18 10:30:44 -0500

    trading mode

--- src/Quant.Infra.Net.Tests/PythonNetTests.cs
+++ src/Quant.Infra.Net.Tests/PythonNetTests.cs
@@ -26,64 +26,64 @@ namespace Quant.Infra.Net.Tests
             _serviceProvider = _serviceCollection.BuildServiceProvider();
         }
 
-        /// <summary>
-        /// 获取单个数据
-        /// </summary>
-        [TestMethod]
-        public void DownloadSymbolData_Should_Work()
-        {
-            var targetPath = AppDomain.CurrentDomain.BaseDirectory + "data\\result";
-            var symbol = "AAPL";
-            var targetFileName = $"{symbol}.csv";
-            var targetFullPathFileName = Path.Combine(targetPath, targetFileName);
-            var pythonFileName = "Functions";
-            var pythonFunctionName = "fetch_and_save_financial_data";
-            var parameterObjs = new List<object>()
-            {
-                targetPath,
-                targetFileName,
-                "2023-1-1",
-                "2024-8-1",
-                "1h",
-                symbol
-            }; // 小时级数据最多730天
-            var pythonDirectories = new List<string>();
-            pythonDirectories.Add(AppDomain.CurrentDomain.BaseDirectory + "Python");
-            var venvPath = @"D:\ProgramData\PythonVirtualEnvs\pair_trading";
-            var pythonDll = "python39.dll";
-            var pyObjectResponse = UtilityService.ExecutePython(pythonFileName, pythonFunctionName, parameterObjs, pythonDirectories, venvPath, pythonDll);
+        ///// <summary>
+        ///// 获取单个数据
+        ///// </summary>
+        //[TestMethod]
+        //public void DownloadSymbolData_Should_Work()
+        //{
+        //    var targetPath = AppDomain.CurrentDomain.BaseDirectory + "data\\result";
+        //    var symbol = "AAPL";
+        //    var targetFileName = $"{symbol}.csv";
+        //    var targetFullPathFileName = Path.Combine(targetPath, targetFileName);
+        //    var pythonFileName = "Functions";
+        //    var pythonFunctionName = "fetch_and_save_financial_data";
+        //    var parameterObjs = new List<object>()
+        //    {
+        //        targetPath,
+        //        targetFileName,
+        //        "2023-1-1",
+        //        "2024-8-1",
+        //        "1h",
+        //        symbol
+        //    }; // 小时级数据最多730天
+        //    var pythonDirectories = new List<string>();
+        //    pythonDirectories.Add(AppDomain.CurrentDomain.BaseDirectory + "Python");
+        //    var venvPath = @"D:\ProgramData\PythonVirtualEnvs\pair_trading";
+        //    var pythonDll = "python39.dll";
+        //    var pyObjectResponse = UtilityService.ExecutePython(pythonFileName, pythonFunctionName, parameterObjs, pythonDirectories, venvPath, pythonDll);
 
-            var targetFileExist = File.Exists(targetFullPathFileName);
-            Assert.IsTrue(targetFileExist);
-        }
+        //    var targetFileExist = File.Exists(targetFullPathFileName);
+        //    Assert.IsTrue(targetFileExist);
+        //}
 
 
-        /// <summary>
-        /// 批量获取数据
-        /// </summary>
-        [TestMethod]
-        public async Task DownloadSymbolsData_Should_Work()
-        {
-            var targetPath = AppDomain.CurrentDomain.BaseDirectory + "data\\result";
-            var sourceDataService = _serviceProvider.GetRequiredService<ITraditionalFinanceSourceDataService>();
-            //var symbols = await sourceDataService.GetSp500SymbolsAsync();
-            var symbols = new List<string>() { "AAPL", "NVDA"};
-            var pythonFileName = "Functions";
-            var pythonFunctionName = "fetch_and_save_financial_data_symbols";
-            var parameterObjs = new List<object>()
-            {
-                symbols.Select(x => x.ToPython()).ToArray(),
-                "2023-1-1",
-                "2024-8-1",
-                targetPath,
-                "1h"
-            }; // 小时级数据最多730天
-            var pythonDirectories = new List<string>();
-            pythonDirectories.Add(AppDomain.CurrentDomain.BaseDirectory + "Python");
-            var venvPath = @"D:\ProgramData\PythonVirtualEnvs\pair_trading";
-            var pythonDll = "python39.dll";
-            UtilityService.ExecutePython(pythonFileName, pythonFunctionName, parameterObjs, pythonDirectories, venvPath, pythonDll);
-        }
+        ///// <summary>
+        ///// 批量获取数据
+        ///// </summary>
+        //[TestMethod]
+        //public async Task DownloadSymbolsData_Should_Work()
+        //{
+        //    var targetPath = AppDomain.CurrentDomain.BaseDirectory + "data\\result";
+        //    var sourceDataService = _serviceProvider.GetRequiredService<ITraditionalFinanceSourceDataService>();
+        //    //var symbols = await sourceDataService.GetSp500SymbolsAsync();
+        //    var symbols = new List<string>() { "AAPL", "NVDA"};
+        //    var pythonFileName = "Functions";
+        //    var pythonFunctionName = "fetch_and_save_financial_data_symbols";
+        //    var parameterObjs = new List<object>()
+        //    {
+        //        symbols.Select(x => x.ToPython()).ToArray(),
+        //        "2023-1-1",
+        //        "2024-8-1",
+        //        targetPath,
+        //        "1h"
+        //    }; // 小时级数据最多730天
+        //    var pythonDirectories = new List<string>();
+        //    pythonDirectories.Add(AppDomain.CurrentDomain.BaseDirectory + "Python");
+        //    var venvPath = @"D:\ProgramData\PythonVirtualEnvs\pair_trading";
+        //    var pythonDll = "python39.dll";
+        //    UtilityService.ExecutePython(pythonFileName, pythonFunctionName, parameterObjs, pythonDirectories, venvPath, pythonDll);
+        //}
 
 
     }
